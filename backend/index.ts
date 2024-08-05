f348329d7a4929495c9173c0884c4e3fe42e84c6
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2024-08-05 22:13:50 +0800

    install primereact and configuring things

--- backend/index.ts
+++ backend/index.ts
@@ -8,57 +8,57 @@ import { DatabaseStorage } from "./database/storage";
 import { CreateServer } from "./server";
 
 const databaseOptions: DatabaseOptions = {
-  sincronize: false,
-  migrationsRun: true,
-  storage: new DatabaseStorage({
-    key: "DATABASE",
-    index: 0,
-  }),
-  entities: ENTITIES,
-  // TODO: Migrations are not found
-  migrations: ["/migrations/*.{ts,js}"],
-  // TODO: logger not working,
-  logger: new ConsoleLogger(false),
+    sincronize: false,
+    migrationsRun: true,
+    storage: new DatabaseStorage({
+        key: "DATABASE",
+        index: 0,
+    }),
+    entities: ENTITIES,
+    // TODO: Migrations are not found
+    migrations: ["/migrations/*.{ts,js}"],
+    // TODO: logger not working,
+    logger: new ConsoleLogger(false),
 };
 
 let db: Database | undefined;
 
 export default Server(
-  async () => {
-    db = new Database(databaseOptions);
-    await db.load();
-    return CreateServer({ database: db });
-  },
-  {
-    init: init([], async () => {
-      try {
-        db = new Database(databaseOptions);
-        await db.init();
-        setNodeServer(CreateServer({ database: db }));
-      } catch (error) {
-        console.error("Error initializing database:", error);
-        throw error;
-      }
-    }),
-    preUpgrade: preUpgrade(() => {
-      try {
-        if (!db) {
-          throw new Error("Database not initialized");
-        }
-
-        db.save();
-      } catch (error) {
-        console.error("Error saving database:", error);
-      }
-    }),
-    postUpgrade: postUpgrade([], async () => {
-      try {
+    async () => {
         db = new Database(databaseOptions);
         await db.load();
-        setNodeServer(CreateServer({ database: db }));
-      } catch (error) {
-        console.error("Error loading database:", error);
-      }
-    }),
-  }
+        return CreateServer({ database: db });
+    },
+    {
+        init: init([], async () => {
+        try {
+            db = new Database(databaseOptions);
+            await db.init();
+            setNodeServer(CreateServer({ database: db }));
+        } catch (error) {
+            console.error("Error initializing database:", error);
+            throw error;
+        }
+        }),
+        preUpgrade: preUpgrade(() => {
+        try {
+            if (!db) {
+                throw new Error("Database not initialized");
+            }
+
+            db.save();
+        } catch (error) {
+            console.error("Error saving database:", error);
+        }
+        }),
+        postUpgrade: postUpgrade([], async () => {
+        try {
+            db = new Database(databaseOptions);
+            await db.load();
+            setNodeServer(CreateServer({ database: db }));
+        } catch (error) {
+            console.error("Error loading database:", error);
+        }
+        }),
+    }
 );
