5f22c75b0e5183391cdf637bfb9674b7827c0880
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2024-08-06 17:28:33 +0800

    configure nested layout and loading file

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-import 'primereact/resources/themes/lara-dark-teal/theme.css'
+import 'primereact/resources/themes/lara-light-teal/theme.css'
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
--- frontend/src/app/trader/page.tsx
+++ frontend/src/app/trader/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+import { Skeleton } from 'primereact/skeleton';
+
+import { useState } from 'react';
+
 export default function Page() {
     return (
         <>
-            <h1>SAMPLE PAGE</h1>            
+            <h1>SAMPLE PAGE</h1>
         </>
-    )
+    );
 }
